68b4e899bd09be2409456b8860a828888760c137
Ayaz Elahi Mullick <ayaz.elahi.mullick@g.bracu.ac.bd>
2025-12-12 19:26:14 +0600

    Sprint 1: Added username to registration and login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,6 +8,13 @@
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
+        <!-- Username -->
+        <div class="mt-4">
+            <x-input-label for="username" :value="__('Username')" />
+            <x-text-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required autocomplete="username" />
+            <x-input-error :messages="$errors->get('username')" class="mt-2" />
+            <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">Username can only contain letters, numbers, dashes and underscores.</p>
+        </div>
 
         <!-- Email Address -->
         <div class="mt-4">
